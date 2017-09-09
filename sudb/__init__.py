813a2b5ad95c0208ec42c451db968730b2af85a9
Hunter Baines <0x68@protonmail.com>
2017-09-09 10:18:36 -0400

    Add info variables to module

--- sudb/__init__.py
+++ sudb/__init__.py
@@ -2,8 +2,15 @@
 
 import sudb
 
+
+__license__ = "GPL3"
+__author__ = __maintainer__ = "Hunter Baines"
+__email__ = "0x68@protonmail.com"
+
+
 def main():
     sudb.main()
+
 
 if __name__ == "__main__":
     main()
