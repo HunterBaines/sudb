81c7dd0a38ee33cee596afd11ba105e4e071b443
Hunter Baines <0x68@protonmail.com>
2017-09-09 02:01:34 -0400

    Add basic setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+# License: GNU GPL version 3
+
+from distutils.core import setup
+
+setup(name='sudb',
+      author='Hunter Baines',
+      author_email='0x68@protonmail.com',
+      packages=['sudb'],
+     )
