6ed9ec6c66d590a8f9992548454b85480fd0fef5
Hunter Baines <0x68@protonmail.com>
2017-10-14 14:24:00 -0400

    Add test of Controller's `help COMMAND`

---
+++ test/test_controller_help.py
@@ -0,0 +1,99 @@
+# Author: Hunter Baines <0x68@protonmail.com>
+# Copyright: (C) 2017 Hunter Baines
+# License: GNU GPL version 3
+
+import os
+import sys
+import tempfile
+import unittest
+
+from sudb.controller import SolverController
+from sudb.board import Board
+
+
+class TestControllerHelp(unittest.TestCase):
+    """Test output of controller command `help COMMAND`.
+
+    """
+
+    # This expects to be run from the project root
+    EXPECTED_OUTPUT_FILE = os.path.join(os.getcwd(), 'test/expected_output/controller_help.out')
+    # How hard would it be to derive matching text from the alias regexes themselves?
+    ALIASES = ['s', 'sm', 'sb', 'sc', 'sr', '555']
+
+
+    @classmethod
+    def setUpClass(cls):
+        cls.maxDiff = None
+
+        # Name it to avoid changes to default name affecting test
+        cls.puzzle = Board(name='test')
+        cls.options = SolverController.Options()
+        # So controller can quit without confirmation
+        cls.options.assume_yes = True
+
+        if os.path.exists(cls.EXPECTED_OUTPUT_FILE):
+            cls.compare_file = open(cls.EXPECTED_OUTPUT_FILE, 'r')
+            cls.output_file = tempfile.NamedTemporaryFile()
+        else:
+            cls.compare_file = None
+            cls.output_file = open(cls.EXPECTED_OUTPUT_FILE, 'w')
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.compare_file is None:
+            print
+            print 'Output written to {}.'.format(cls.EXPECTED_OUTPUT_FILE)
+            print 'Future tests will compare actual output to this file.'
+        else:
+            cls.compare_file.close()
+        cls.output_file.close()
+
+
+    def test_help(self):
+        self.assertEqual(len(self.ALIASES), len(SolverController.Options().aliases))
+
+        # Add help commands for non-aliased commands
+        command_queue = []
+        for command in SolverController(self.puzzle).cmd.commands:
+            command_queue.append('help ' + command)
+            if command.startswith('help'):
+                # This can introduce duplicates, but they'll be removed before running
+                command_queue.append(command)
+
+        # Add help commands for aliased commands
+        for alias in self.ALIASES:
+            command_queue.append('help ' + alias)
+
+        # Remove duplicates and standardize order
+        command_queue = list(set(command_queue))
+        command_queue.sort()
+
+        # So the controller will exit
+        command_queue.append('quit')
+
+        # Run commands
+        controller = SolverController(self.puzzle, command_queue=command_queue,
+                                      options=self.options)
+        self.redirect_output()
+        controller.solve()
+        self.reset_output()
+
+        if self.compare_file is None:
+            # Output already written to `EXPECTED_OUTPUT_FILE`; nothing else to do
+            return
+
+        # Compare output
+        self.output_file.seek(0)
+        output_lines = self.output_file.read().splitlines()
+        compare_lines = self.compare_file.read().splitlines()
+        self.assertEqual(output_lines, compare_lines)
+
+
+    def redirect_output(self):
+        sys.stdout = self.output_file
+        sys.stderr = self.output_file
+
+    def reset_output(self):
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
