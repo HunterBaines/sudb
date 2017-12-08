f78f29e9d904649aa5b103d5e38984acdd4c75e0
Hunter Baines <0x68@protonmail.com>
2017-12-07 21:35:08 -0500

    Update test outcome in comment; wrap long comments

--- test/test_generator_uniqueness.py
+++ test/test_generator_uniqueness.py
@@ -11,20 +11,23 @@ class TestGeneratorUniqueness(unittest.TestCase):
     """Test if a range of seeds ever generate the same puzzle.
 
     """
-
     START_SEED = 0
     # A small amount so this doesn't hold back `python -m unittest discover`
     END_SEED = 5
 
     def test_uniqueness(self):
-        # NB: Testing this well requires generating from many seeds, which takes a long time
-        # (on my machine, running this test with 1001 seeds takes about 11.3 minutes). As of
-        # 2017 November 6, the test passes for seeds between 0 and 10000 inclusive.
+        # NB: Testing this well requires generating from many seeds, which
+        # takes a long time (on my machine, running this test with 1001
+        # seeds takes about 11.3 minutes). The last change to the RNG
+        # occurred on 2017 December 7 in commits a428a04a and 9d2f13e; as
+        # of those commits and that date, this passes for seeds between 0
+        # and 10000 inclusive
         puzzle_hash_dict = {}
         repeated_puzzles = []
 
         for seed in range(self.START_SEED, self.END_SEED + 1):
-            # A waste of time to duplicate puzzle; a waste of space to store it in dict
+            # A waste of time to duplicate puzzle; a waste of space to
+            # store it in dict
             puzzle_hash = hash(generator.generate(seed))
             try:
                 puzzle_hash_dict[puzzle_hash].add(seed)
@@ -37,5 +40,6 @@ class TestGeneratorUniqueness(unittest.TestCase):
         self.assertEqual(repeated_seeds, [])
 
         max_unique_puzzles = self.END_SEED - self.START_SEED + 1
-        # Designed to show number of unique puzzles relative to number possible if assertion fails
+        # Designed to show number of unique puzzles relative to number
+        # possible if assertion fails
         self.assertEqual(len(puzzle_hash_dict), max_unique_puzzles)
