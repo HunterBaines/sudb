67b59a5e4f5ba45898c5ec8e38037c2a66f76cc0
Hunter Baines <0x68@protonmail.com>
2017-12-08 19:55:43 -0500

    Add test of `set explainsteps`, minor improvements
    Minor improvements: add more comments in test output, remove a few
    unnecessary commands

--- test/test_controller_set.py
+++ test/test_controller_set.py
@@ -9,13 +9,13 @@ import os
 from test.output_tester import OutputTester
 from sudb.controller import SolverController
 from sudb.board import Board
+from sudb.solver import Solver
 
 
 class TestControllerSet(OutputTester):
     """Test output of controller commands involving set.
 
     """
-
     # This expects to be run from the project root
     EXPECTED_OUTPUT_FILE = os.path.join(os.getcwd(),
                                         'test/expected_output/controller_set.out')
@@ -42,9 +42,18 @@ class TestControllerSet(OutputTester):
 
 
     def test_set(self):
-        command_queue = []
+        temp_solver = Solver(self.puzzle.duplicate())
+        move1_row, move1_col = temp_solver.step()
+        move1_box, _ = Board.box_containing_cell(move1_row, move1_col)
+        move1_num = temp_solver.puzzle.get_cell(move1_row, move1_col)
 
-        command_queue.append('# first try `set` with no subcommand')
+        # Adjust zero-indexed locations returned by `temp_solver`
+        move1_row += 1
+        move1_col += 1
+        move1_box += 1
+
+        command_queue = []
+        command_queue.append('# test `set` with no subcommand')
         command_queue.append(self.SET_CMD)
 
         command_queue.append('# test `set ascii`')
@@ -54,7 +63,7 @@ class TestControllerSet(OutputTester):
         command_queue.append('print')
 
         command_queue.append('# test `set guessbreak`')
-        command_queue.append('step {}'.format(self.MOVE_OF_FIRST_GUESS-1))
+        command_queue.append('step {} # right before guess'.format(self.MOVE_OF_FIRST_GUESS-1))
         command_queue.append('check move_before_guess')
         command_queue.append('step 2')
         command_queue.append('restart move_before_guess')
@@ -63,14 +72,32 @@ class TestControllerSet(OutputTester):
         command_queue.append('restart')
         command_queue.append('{} guessbreak # turn off'.format(self.SET_CMD))
 
+        command_queue.append('# test `set explainsteps`')
+        command_queue.append('{} explainsteps # turn on'.format(self.SET_CMD))
+        command_queue.append('# make sure explanation is printed for `step`/`stepm`')
+        command_queue.append('step')
+        command_queue.append('stepm {} {} {}'.format(move1_row, move1_col, move1_num))
+        command_queue.append('unstep 2')
+        command_queue.append('# make sure explanation is printed for other `step` variants')
+        command_queue.append('stepr {}'.format(move1_row))
+        command_queue.append('unstep')
+        command_queue.append('stepc {}'.format(move1_col))
+        command_queue.append('unstep')
+        command_queue.append('stepb {}'.format(move1_box))
+        command_queue.append('unstep')
+        command_queue.append('# make sure explanation is printed for `finish`')
+        command_queue.append('break {} {} # avoid excess output'.format(move1_row, move1_col))
+        command_queue.append('finish')
+        command_queue.append('{} explainsteps # turn off'.format(self.SET_CMD))
+
         command_queue.append('# test `set markview`')
-        command_queue.append('mark 1 1 9')
+        command_queue.append('{} markview # turn on'.format(self.SET_CMD))
+        command_queue.append('mark 1 1 9  # add some candidates')
         command_queue.append('mark 9 9 9')
-        for effect in ('on', 'off'):
-            command_queue.append('{} markview # turn {}'.format(self.SET_CMD, effect))
-            command_queue.append('step')
-            command_queue.append('explain')
-            command_queue.append('unstep')
+        command_queue.append('step')
+        command_queue.append('explain')
+        command_queue.append('unstep')
+        command_queue.append('{} markview # turn off'.format(self.SET_CMD))
 
         command_queue.append('# test `set prompt`')
         command_queue.append('set prompt sudb# ')
@@ -82,13 +109,13 @@ class TestControllerSet(OutputTester):
 
         command_queue.append('# test `set width`')
         command_queue.append('{} width 80 # set wide'.format(self.SET_CMD))
-        command_queue.append('print marks')
-        command_queue.append('help step')
+        command_queue.append('print marks  # show wide puzzle')
+        command_queue.append('help step    # show wide wrapped text')
         command_queue.append('{} width 60 # set narrow'.format(self.SET_CMD))
-        command_queue.append('print marks')
-        command_queue.append('{} ascii # show ascii version'.format(self.SET_CMD))
-        command_queue.append('print marks')
-        command_queue.append('help step')
+        command_queue.append('print marks  # show narrow puzzle')
+        command_queue.append('{} ascii'.format(self.SET_CMD))
+        command_queue.append('print marks  # show narrow ascii puzzle')
+        command_queue.append('help step    # show narrow wrapped text')
         command_queue.append('{} width 0  # restore default'.format(self.SET_CMD))
 
         command_queue.append('# test passing bad arguments')
